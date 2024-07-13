c59c1f20aac13f856a518e7d700b1abe77a1127f
White Christopher <141556256+0x00whitejsx@users.noreply.github.com>
2024-07-13 16:27:34 +0000

    connect the update ' git push

--- src/component/Project.jsx
+++ src/component/Project.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './About.css'
-import Bidder from './form/Bidder'
+// import Bidder from './form/Bidder'
+import Bid from './form/bid'
 
 
 
 function Project() {
   return (
     <div className='w-full h-[100vh]  md:pl-10 md:pr-10'>
-      <Bidder />
+      <Bid />
       </div>
   )
 }
