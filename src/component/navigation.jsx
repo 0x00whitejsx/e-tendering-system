e11c3a88a790e2adaaf6325d703662cf1c3cd1c6
White Christopher <141556256+0x00whitejsx@users.noreply.github.com>
2024-07-13 16:43:43 +0000

    connect the update ' git push

--- src/component/navigation.jsx
+++ src/component/navigation.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 function Navigation() {
   return (
-    <div className='w-full  top-0 p-8 bg-g'>
+    <div className='w-full top-0 p-8 bg-gray-800'>
         <div className='flex justify-between items-end'>
             <div>
-              <p className='text-slate-400 font-bold invert-0'>ADBP-ES</p>
+              <p className='text-slate-400 font-bold'>ADBP-ES</p>
             </div>
-            <div>
-        <a className='p-5 text-xlg text-green-700 hover:text-green-900' href='/'> [ Home ]</a>
-        <a className='p-5 text-xlg text-green-700 hover:text-green-900' href='Bid'> [ Bid ]</a>
-        <a className='p-5 text-xlg text-green-700 hover:text-green-900' href='Projects'> [ view Bidder ]</a>
-        <a className='p-5 text-xlg text-green-700 hover:text-green-900' href='Skills'> [ Open contract ]</a>
-        <a className='p-5 text-xlg text-green-700 hover:text-green-900' href='Skills'> [ View  ]</a>
-        <a className='p-5 text-xlg text-green-700 hover:text-green-900' href='Policy'> [ Policy  ]</a>
-        <a className='p-5 text-xlg text-green-700 hover:text-green-900' href='About'> [ About ]</a>
-      
-        </div>
+            <div className='flex space-x-5'>
+              <a className='text-lg text-green-700 hover:text-green-900' href='/' aria-label='Home'>[ Home ]</a>
+              <a className='text-lg text-green-700 hover:text-green-900' href='/Bid' aria-label='Bid'>[ Bid ]</a>
+              <a className='text-lg text-green-700 hover:text-green-900' href='/Projects' aria-label='View Bidder'>[ View Bidder ]</a>
+              <a className='text-lg text-green-700 hover:text-green-900' href='/OpenContract' aria-label='Open Contract'>[ Open Contract ]</a>
+              <a className='text-lg text-green-700 hover:text-green-900' href='/View' aria-label='View'>[ View ]</a>
+              <a className='text-lg text-green-700 hover:text-green-900' href='/Policy' aria-label='Policy'>[ Policy ]</a>
+              <a className='text-lg text-green-700 hover:text-green-900' href='/About' aria-label='About'>[ About ]</a>
+            </div>
         </div>
     </div>
-  )
+  );
 }
 
-export default Navigation
+export default Navigation;
