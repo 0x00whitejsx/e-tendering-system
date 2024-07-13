24969e60173aab299cb7318351da0212d5d70be5
White Christopher <141556256+0x00whitejsx@users.noreply.github.com>
2024-07-13 16:45:40 +0000

    connect the update ' git push

--- src/component/navigation.jsx
+++ src/component/navigation.jsx
@@ -5,16 +5,16 @@ function Navigation() {
     <div className='w-full top-0 p-8 bg-gray-800'>
         <div className='flex justify-between items-end'>
             <div>
-              <p className='text-slate-400 font-bold'>ADBP-ES</p>
+              <p className='text-white font-bold'>ADBP-ES</p>
             </div>
             <div className='flex space-x-5'>
-              <a className='text-lg text-green-700 hover:text-green-900' href='/' aria-label='Home'>[ Home ]</a>
-              <a className='text-lg text-green-700 hover:text-green-900' href='/Bid' aria-label='Bid'>[ Bid ]</a>
-              <a className='text-lg text-green-700 hover:text-green-900' href='/Projects' aria-label='View Bidder'>[ View Bidder ]</a>
-              <a className='text-lg text-green-700 hover:text-green-900' href='/OpenContract' aria-label='Open Contract'>[ Open Contract ]</a>
-              <a className='text-lg text-green-700 hover:text-green-900' href='/View' aria-label='View'>[ View ]</a>
-              <a className='text-lg text-green-700 hover:text-green-900' href='/Policy' aria-label='Policy'>[ Policy ]</a>
-              <a className='text-lg text-green-700 hover:text-green-900' href='/About' aria-label='About'>[ About ]</a>
+              <a className='text-lg text-white font-bold hover:text-green-400' href='/' aria-label='Home'>[ Home ]</a>
+              <a className='text-lg text-white font-bold hover:text-green-400' href='/Bid' aria-label='Bid'>[ Bid ]</a>
+              <a className='text-lg text-white font-bold hover:text-green-400' href='/Projects' aria-label='View Bidder'>[ View Bidder ]</a>
+              <a className='text-lg text-white font-bold hover:text-green-400' href='/OpenContract' aria-label='Open Contract'>[ Open Contract ]</a>
+              <a className='text-lg text-white font-bold hover:text-green-400' href='/View' aria-label='View'>[ View ]</a>
+              <a className='text-lg text-white font-bold hover:text-green-400' href='/Policy' aria-label='Policy'>[ Policy ]</a>
+              <a className='text-lg text-white font-bold hover:text-green-400' href='/About' aria-label='About'>[ About ]</a>
             </div>
         </div>
     </div>
