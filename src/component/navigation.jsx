945d61c2c9e52335edbdab59960c139b1e254b26
White Christopher <141556256+0x00whitejsx@users.noreply.github.com>
2024-07-13 12:47:12 +0000

    adaed the file

--- src/component/navigation.jsx
+++ src/component/navigation.jsx
@@ -5,7 +5,7 @@ function Navigation() {
     <div className='w-full  top-0 p-8 bg-g'>
         <div className='flex justify-between items-end'>
             <div>
-              <p className='text-slate-400 font-bold invert-0'>statetbidder.com</p>
+              <p className='text-slate-400 font-bold invert-0'>ADBP-ES</p>
             </div>
             <div>
         <a className='p-5 text-sm text-green-700 hover:text-green-900' href='/'> [ Home ]</a>
